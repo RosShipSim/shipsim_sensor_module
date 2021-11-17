b503e023c68025f9963521800a02a340c32a0a91
Taiga MITSUYUKI <mitsuyuki-taiga-my@ynu.ac.jp>
2021-11-17 18:57:50 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'shipsim_sensor_module'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mitsuyuki',
+    maintainer_email='mitsuyuki-taiga-my@ynu.ac.jp',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
